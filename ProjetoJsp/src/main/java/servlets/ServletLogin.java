40451f114fe088df071b2db82a05e5fb5bb3ab8d
Jr.Nascimento <Jr.Nascimento@DESKTOP-85521FT>
2023-02-15 12:19:18 -0300

    Implementação do layout da pagina principal. Codificação da ação do botão logout.

--- ProjetoJsp/src/main/java/servlets/ServletLogin.java
+++ ProjetoJsp/src/main/java/servlets/ServletLogin.java
@@ -25,7 +25,22 @@ public class ServletLogin extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		doPost(request, response);
+		
+		String acao= request.getParameter("acao");
+		if(acao!=null && !acao.isEmpty() && !acao.equalsIgnoreCase("logout") ) {
+			request.getSession().invalidate();//invalida a seção.
+			
+			RequestDispatcher direcionar= request.getRequestDispatcher("index.jsp");
+			direcionar.forward(request, response);
+			
+			
+		}else {
+			
+			doPost(request, response);
+			
+		}
+		
+		
 
 	}
 
